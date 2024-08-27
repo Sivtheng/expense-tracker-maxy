0219c7e974ebaa063ca7eb2f7dba84181c23eaec
Sivtheng <sivtheng25@gmail.com>
2024-08-27 13:01:45 +0700

    basic layouts for login and expense page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :require_login, except: [:new, :create]
+
+  private
+
+  def require_login
+    unless session[:user_id]
+      redirect_to login_path, alert: 'You must be logged in to access this application'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "expenses/index"
+  get "expenses/show"
+  get "expenses/new"
+  get "expenses/edit"
+  # Route for login and logout
+  root 'sessions#new'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destory'
+  resources :expenses
+  resources :categories
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
